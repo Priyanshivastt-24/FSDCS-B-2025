313f58e82b10fdebfb3e50d9564826e314ebfd9f
Priyanshivastt-24 <priyanshi.vastt24@gmail.com>
2025-11-26 10:26:27 +0530

    ap fetch and dashboard

--- ReactWork/reactviteApp2/vite-project/src/pages/Dashboard.jsx
+++ ReactWork/reactviteApp2/vite-project/src/pages/Dashboard.jsx
@@ -1,19 +1,37 @@
 import React, { use, useEffect, useState } from 'react'
 
 function Dashboard() {
-
-  const [counter,setCounter] = useState(10);
-  const[pointer,setPointer]=useState(100);
-  useEffect(() => {
-    console.log(counter);
-    console.log(pointer)
-  },[counter,pointer])
+  const[data,setData]=useState([]);
+  useEffect(()=>{
+      async function fetchData(){
+        const serverresponse = await fetch('https://fakestoreapi.com/products');
+        const jsonResponse=await serverresponse.json();
+        console.log(jsonResponse);
+        setData(jsonResponse);
+       }
+       fetchData();
+  },[])
+ 
   return (
     <div>
-      <h2>Counter value={counter}</h2>
-      <h2>Pointer value{pointer}</h2>
-      <button onClick={()=>setCounter(counter+10)}>Counter</button>
-      <button onClick={()=>setPointer(pointer-10)}>Pointer</button>
+      {
+        data.length === 0?(<h2>Data could not fetch</h2>):(
+          <div>
+            {
+            data.map((ele)=>(
+              <div style={{border:"2px solid gray",height :'300px',width:'300px'}} >
+                <img src={ele.image} height={100} width={100}></img>
+                <h3>{ele.title}</h3>
+                <button >Add to Cart</button>
+              </div>
+            ))
+          }
+          </div>
+        )
+      }
+      {/* {
+        JSON.stringify(data)
+      } */}
     </div>
   )
 }

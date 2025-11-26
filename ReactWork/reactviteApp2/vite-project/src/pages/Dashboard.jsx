131be6a8f0462813c116174eb5ee15fe60db4aaf
Priyanshivastt-24 <priyanshi.vastt24@gmail.com>
2025-11-26 09:57:31 +0530

    useeffect usecase

--- ReactWork/reactviteApp2/vite-project/src/pages/Dashboard.jsx
+++ ReactWork/reactviteApp2/vite-project/src/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 
 function Dashboard() {
+
+  const [counter,setCounter] = useState(10);
+  const[pointer,setPointer]=useState(100);
+  useEffect(() => {
+    console.log(counter);
+    console.log(pointer)
+  },[counter,pointer])
   return (
     <div>
-      
+      <h2>Counter value={counter}</h2>
+      <h2>Pointer value{pointer}</h2>
+      <button onClick={()=>setCounter(counter+10)}>Counter</button>
+      <button onClick={()=>setPointer(pointer-10)}>Pointer</button>
     </div>
   )
 }
